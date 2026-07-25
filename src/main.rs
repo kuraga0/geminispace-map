b8ffd6c615470284f00877907ed647af62c90123
Andrew <gus123tractor@gmail.com>
2026-07-25 22:31:15 +0400

    init

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use gmi::{protocol::StatusCode, request::RequestError, *};
+use std::convert::TryFrom;
+
+fn request_page(url: &str) -> Result<std::vec::Vec<u8>, StatusCode> {
+	let mut url = url::Url::try_from(url).unwrap();
+	println!("Making response with URL: {}", url);
+	loop {
+		let response = request::make_request(&url)?;
+		match response.status {
+			protocol::StatusCode::Redirect(c) => {
+				println!("Redirect! Code: {} with meta {}", c, response.meta);
+				url = url::Url::try_from(response.meta.as_str()).unwrap();
+				println!("New URL: {}", url);
+			}
+			protocol::StatusCode::Success(c) => {
+				println!("Success! Code: {} with MIME type: {}", c, response.meta);
+				return Ok(response.data.clone());
+			}
+			s => return Err(s),
+		}
+	}
+}
+
+fn main() {
+	request_page("gemini://station.martinrue.com");
+}
