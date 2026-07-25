3642534ae1c3b83616c2e00137a0718ca5b2aa11
Andrew <gus123tractor@gmail.com>
2026-07-25 23:42:48 +0400

    some kind of error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-use gmi::*;
+use gmi::{protocol::StatusCode, *};
 use std::convert::TryFrom;
 
-fn request_page(url: &str) -> Result<String, request::RequestError> {
+#[derive(Debug)]
+struct GeminiError {
+	status: StatusCode,
+	meta: String,
+}
+
+impl std::fmt::Display for GeminiError {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{:?}: {}", self.status, self.meta)
+	}
+}
+
+impl std::error::Error for GeminiError {}
+
+fn request_page(url: &str) -> Result<String, Box<dyn std::error::Error>> {
 	let mut url = url::Url::try_from(url).unwrap();
 	println!("Making response with URL: {}", url);
 	loop {
@@ -16,13 +30,33 @@ fn request_page(url: &str) -> Result<String, request::RequestError> {
 				println!("Success! Code: {} with MIME type: {}", c, response.meta);
 				return Ok(String::from_utf8_lossy(&response.data).into_owned());
 			}
-			s => panic!("Unknown status code: {:?}", s),
+      StatusCode::PermanentFailure(c) => {
+        return Err(Box::new(GeminiError { status: StatusCode::PermanentFailure(c), meta: response.meta }));
+      }
+			// s => return Err(format!("Unknown status code: {:?}", s).into()),
+			s => return Err(format!("Unknown status code: {:?}", s).into()),
 		}
 	}
 }
 
 fn main() {
-  let page = request_page("gemini://station.martinrue.com").unwrap();
+  	let page = match request_page("gemini://kennedy.gemi.dev/dsfg2fdwe") {
+		Ok(page) => page,
+		Err(e) => {
+			eprintln!("Error: {e}");
+			return;
+		}
+	};
 
-  println!("{page}");
+	// println!("{page}");
+
+	let gemtext_nodes = gemtext::parse_gemtext(page.as_str());
+
+	println!("0: {}", &gemtext_nodes[0]);
+
+	if let gemtext::GemtextNode::Heading(s) = &gemtext_nodes[0] {
+		println!("{s}");
+	} else {
+		println!("Incorrect type!");
+	}
 }
